ebfa3f93f06ab70c9cedd02dbf412038632a9254
mortifia <amela57290@gmail.com>
2017-08-31 16:51:06 +0200

    koko

--- main.py
+++ main.py
@@ -12,10 +12,11 @@ version = 0.0010
 version = float(version)
 urlSer = 'http://ipr-3d.com:8080'
 
-
-
 if os.path.exists("/var/egg-force-one"):
     os.chdir("/var/egg-force-one")
+
+
+
 
 # partage de données entre threads (ne pas modifier)
 stopAll = 0# ne pas toucher
