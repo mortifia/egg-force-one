7ec7ec0540875e8a33a0d4e19db9f21d37f3a689
mortifia <amela57290@gmail.com>
2017-10-23 01:48:05 +0200

    ...

--- control.py
+++ control.py
@@ -93,6 +93,7 @@ class Control (Thread):
         if (self.sysVar.printStatut == 1):
             if (self.countIn == self.countOut):
                 tmp = self.folder.readline()
+                self.msgTerminal(2, "onPrint add :" + tmp)
                 self.addGcode(tmp)
                 pass
             pass
