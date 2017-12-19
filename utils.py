5631cccca2481289cfbd518b54d098768a7f8316
mortifia <amela57290@gmail.com>
2017-12-19 12:40:03 +0100

    licence

--- utils.py
+++ utils.py
@@ -1,12 +1,28 @@
 # -*- coding: utf-8 -*-
 """
-Created on Mon Sep 25 18:09:26 2017
+    This file is part of egg-force-one.
 
-@author: 54386
+    egg-force-one is free software: you can redistribute it and/or modify
+    it under the terms of the GNU General Public License as published by
+    the Free Software Foundation, either version 3 of the License, or
+    (at your option) any later version.
+
+    Foobar is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU General Public License for more details.
+
+    You should have received a copy of the GNU General Public License
+    along with egg-force-one.  If not, see <http://www.gnu.org/licenses/>. 2
+
+    ############################################################################
+
+    Created on Mon Sep 25 16:24:39 2017
+    @author: CASAL Guillaume
 """
 
 def allVarModule (module):
-    """    
+    """
     permet de connaitre les variable et les valeur dans un module
     """
     tempData = vars(module)
